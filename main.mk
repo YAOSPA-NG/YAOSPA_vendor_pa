cb80814211b889021bd95a1a78a84342650de59c
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-08-25 23:39:26 +0200

    Include vendor_yaosp
    Change-Id: I2c011cf73ac7e44c3a716ce9587edfe11da20965
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- main.mk
+++ main.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include vendor YAOSP
+include vendor/yaosp/configs/yaosp_main.mk
+
 export VENDOR := pa
 
 # Include versioning information
